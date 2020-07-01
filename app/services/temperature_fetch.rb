3617902b47e5842375286548b0d48c5c4f9f3fce
goonberger <r-pawlicki@wp.pl>
2020-07-01 14:15:23 +0200

    rescued temperature fetch errors are now logged

--- app/services/temperature_fetch.rb
+++ app/services/temperature_fetch.rb
@@ -4,13 +4,9 @@ class TemperatureFetch
     params = { q: city, appid: ENV['OPENWEATHER_API_KEY'], units: "metric" }
     begin
       response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather", query: params)
-      temperature = response.fetch("main").fetch("temp")
-    rescue StandardError => e
-      if response && response["message"]
-        p "ERROR: #{response["message"]}"
-      else
-        p "ERROR: #{e.message}"
-      end
+      temperature = response.fetch("main").fetch("temp") if response["main"]
+    rescue StandardError, HTTParty::Error => e
+      Rails.logger.debug "ERROR: #{e.message}"
     end
     temperature
   end
